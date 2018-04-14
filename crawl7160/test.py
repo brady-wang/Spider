cc117a9e35978fedbcfa5f9da46b0b92f6e37a25
brady.wang <brady.wang@qq.com>
2018-04-14 15:23:10 +0800

    d

---
+++ crawl7160/test.py
@@ -0,0 +1,25 @@
+import random
+
+
+def mkdir(path):
+    # 引入模块
+    import os
+    # 去除首位空格
+    path = path.strip()
+    # 去除尾部 \ 符号
+    path = path.rstrip("\\")
+
+    # 判断路径是否存在
+    # 存在     True
+    # 不存在   False
+    isExists = os.path.exists(path)
+
+    # 判断结果
+    if not isExists:
+        os.makedirs(path)
+        return True
+    else:
+        return False
+
+
+print (random.randint(12, 20))
